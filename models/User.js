5b13164c89dce422ccfdafcf927db7410cef834a
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2021-10-05 08:38:12 +0200

    first commit

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password : {
+        type: String,
+        required: true
+    },
+
+    date : {
+        type: Date,
+        default: Date.now
+    },
+    
+    role:{
+        type: String,
+        require:true,
+        default: 'user'
+    }
+
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
